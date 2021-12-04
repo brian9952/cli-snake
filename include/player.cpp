bb9d432d45ea9540581a06ee3673758439e8ab00
brian9952 <fbriann@gmail.com>
2021-12-04 12:05:44 +0700

    create player class

---
+++ include/player.cpp
@@ -0,0 +1,10 @@
+#include "player.h"
+
+Player::Player(WINDOW * mainWin_param, WINDOW * sideWin_param){
+    mainWin = mainWin_param;
+    sideWin = sideWin_param;
+}
+
+void Player::movePlayer(char input){
+
+}
---
+++ include/player.h
@@ -0,0 +1,22 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+#include<ncurses.h>
+#include<iostream>
+
+class Player {
+
+    private:
+        WINDOW * mainWin;
+        WINDOW * sideWin;
+        int yPos, xPos;
+        int length;
+        char heading[];
+
+    public:
+        Player(WINDOW * mainWin_param, WINDOW * sideWin_param);
+        void movePlayer(char input);
+        ~Player();
+
+};
+
+#endif
